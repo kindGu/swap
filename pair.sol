478bf221b2cb6dfe74d5dde1a44c605b7fcd33b3
kindGu <31135423+kindGu@users.noreply.github.com>
2022-09-11 21:26:26 +0800

    Update pair.sol

--- pair.sol
+++ pair.sol
@@ -69,6 +69,13 @@ contract Pair is iPair, ERC20 {
         if (_kLast != 0) {
             uint rootK = Math.sqrt(uint(_reserve0).mul(_reserve1));
             uint rootKLast = Math.sqrt(_kLast);
+            if (rootK > rootKLast) {
+                    uint numerator = totalSupply.mul(rootK.sub(rootKLast));
+                    uint denominator = rootK.add(rootKLast);
+                    //1/2
+                    uint liquidity = numerator / denominator;
+                    if (liquidity > 0) _mint(feeTo, liquidity);
+            }
         }
     }
     
