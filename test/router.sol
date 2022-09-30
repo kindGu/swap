0afb4260ccf34865ace6f9dc2f43cc77ac26adb2
WangXiHua <31135423+kindGu@users.noreply.github.com>
2022-09-30 18:48:15 +0800

    Update router.sol

--- test/router.sol
+++ test/router.sol
@@ -1,1 +1,18 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
 
+import "./iWTH.sol";
+import "./IERC20.sol";
+import "./iFactory.sol";
+import "./SafeMath.sol";
+
+contract Router{
+  using SafeMath for uint;
+  address public immutable override factory;
+  address public immutable override WETH;
+  
+  constructor(address _factory, address _WETH) public {
+        factory = _factory;
+        WETH = _WETH;
+    }
+}
